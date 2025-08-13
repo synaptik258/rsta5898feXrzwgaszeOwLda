39e9bd3267253b310dc5bbcebb4ae76c27bdac66
synaptik258 <58080315+synaptik258@users.noreply.github.com>
2025-08-13 22:20:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ TELEGRAM_CHAT_ID = 6202982315
 HEADERS = {
     "User-Agent": UserAgent().random
 }
-SEARCH_URL = "https://olx.pl/oferty/q-maszyna-do-szycia/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Border%5D=relevance%3Adesc"
+SEARCH_URL = "https://olx.pl/oferty/q-maszyna-do-szycia/?search%5Border%5D=relevance:desc&search%5Bfilter_float_price:from%5D=free"
 BASE_URL = "https://www.olx.pl"
 
 SEEN_FILE = "seen.json"
